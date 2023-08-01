885e3827b2bdd0a29b32dee3b3cad3131b1147bc
codespace555 <namansanjaykumar@gmail.com>
2023-08-01 13:18:54 +0530

    all done

--- TIC-TAC-TOE/src/App.jsx
+++ TIC-TAC-TOE/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Card from "./components/Card/Card";
+
 import Icon from "./components/Icon/Icon";
 import Grid from "./components/Grid/Grid";
 import "./App.css";
