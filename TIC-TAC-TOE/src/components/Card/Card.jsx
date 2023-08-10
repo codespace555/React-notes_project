8437f7fd9886b9cd1971157aaa8a95fd7e2429d4
codespace555 <namansanjaykumar@gmail.com>
2023-08-10 15:46:08 +0530

    card edit

--- TIC-TAC-TOE/src/components/Card/Card.jsx
+++ TIC-TAC-TOE/src/components/Card/Card.jsx
@@ -1,18 +1,12 @@
-import Icon from "../Icon/Icon";
-import "./Card.css";
-function Card({ onPlay,player,index }) {
-    let icon = <Icon />
- if(player == "X"){
-icon = <Icon name={"cross"} />
- }else if(player == "O"){
-    icon = <Icon name={"circle"} />
- }
-
-  return (
-    <div className="card" onClick={() =>  player == ""  && onPlay(index)}>
-     {icon}
-    </div>
-  );
+import Icon from "../Icon/Icon"
+function Card() {
+    return(
+        <>
+        <div className="card">
+           <Icon />
+        </div>
+        </>
+    )
 }
 
 export default Card;
